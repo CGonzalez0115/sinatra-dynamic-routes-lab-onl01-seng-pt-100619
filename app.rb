c4f961463b73b3d0c2e53eaf94a2f7271e843a23
Christina Gonzalez <cmgamway@gmail.com>
2020-01-30 04:27:40 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,10 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+
+  get '/reversename/:name' do
+    @revers_name = params[:name].reverse
+    @reversename
+  end
 
 end
